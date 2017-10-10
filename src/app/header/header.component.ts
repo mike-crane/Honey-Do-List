cd74ea5ec1bd7221883c5fc333839e616ed7a373
mike crane <mike.c0321@gmail.com>
2017-10-09 23:13:07 -0400

    add dummy data to mock up request feature

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styles: [`
+      .navbar-default {
+        background-color: #8BD0F3;
+      }
+    `]
 })
 
 export class HeaderComponent {
--- src/app/requests/request-list/request-list.component.ts
+++ src/app/requests/request-list/request-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Request } from '../request.model';
+
 @Component({
   selector: 'app-request-list',
   templateUrl: './request-list.component.html',
   styleUrls: ['./request-list.component.css']
 })
 export class RequestListComponent implements OnInit {
+  requests: Request[] = [
+    new Request('Leaky Faucet', 'The faucet in the guest bathroom is dripping', 'https://c1.staticflickr.com/3/2713/4362261135_a199724297.jpg'), 
+    new Request('Leaky Faucet', 'The faucet in the guest bathroom is dripping', 'https://c1.staticflickr.com/3/2713/4362261135_a199724297.jpg')
+  ];
 
   constructor() { }
 
